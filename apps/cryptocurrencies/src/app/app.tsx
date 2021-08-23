ad9404799ccc9726aba2774c37ebe6a534629194
Daniel Camilo <dacamoty@gmail.com>
2021-08-22 23:16:57 -0500

    Services added

--- apps/cryptocurrencies/src/app/app.tsx
+++ apps/cryptocurrencies/src/app/app.tsx
@@ -1,12 +1,23 @@
-import { Spin } from 'antd'
+import { Spin } from 'antd';
+import { useEffect } from 'react';
+import {
+  getTickers,
+  getCoinDetail,
+  getSocialStats,
+} from '../services/coinlore';
 
 const App = () => {
+  useEffect(() => {
+    // getTickers();
+    // getCoinDetail("90")
+    getSocialStats('90');
+  }, []);
   return (
     <div>
       NX Installed with Antd
       <Spin />
     </div>
-  )
-}
+  );
+};
 
 export default App;
